3d4fc0406be870f76a06c536077e7711a464f162
Karan Gole <68986065+karan-gole8223@users.noreply.github.com>
2025-01-17 10:26:55 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'karan' );
 define( 'DB_PASSWORD', 'Cool@123' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'healthykid-a95262ffb6-wpdbserver.mysql.database.azure.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
